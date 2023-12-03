7eb71a6ce2f9b771c08945d3bbc96f3e58f15d03
Anthony Chablov <achablov@myseneca.ca>
2023-12-03 14:22:21 -0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import MainLayout from "@/components/Main/MainLayout";
+
+export default function Home() {
+  return (
+    <main className="">
+      <MainLayout />
+    </main>
+  );
+}
