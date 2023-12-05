218798c63b637ca3803a2ea59f9b5cede397454d
Anthony Chablov <achablov@myseneca.ca>
2023-12-05 15:43:30 -0500

    update tool tip modal

--- src/components/Common/Modal/Modal.tsx
+++ src/components/Common/Modal/Modal.tsx
@@ -68,36 +68,38 @@ const Modal = () => {
         defaultOpen={toggleModal}
         onOpenChange={() => setToggleModal(!toggleModal)}
       >
-        <DialogContent className="p-6 ">
+        <DialogContent className="p-4 md:p-6">
           <DialogHeader>
-            <DialogTitle className="text-slate-800 text-xl">
+            <DialogTitle className="text-lg md:text-xl mb-2">
               Trade Import Utility Conversion Tool
             </DialogTitle>
-            <DialogDescription className="text-base">
+            <DialogDescription className="text-sm md:text-base text-left mb-4">
               Welcome to the Trade Import Utility Conversion Tool, a tool
               designed to streamline the process of converting and importing
               your trade data. Follow these steps to ensure a smooth experience:
             </DialogDescription>
             <Separator />
           </DialogHeader>
-          <div className=" max-h-96 overflow-y-scroll space-y-8 w-full pt-2">
+          <div className="max-h-96 overflow-y-scroll space-y-4 w-full">
             {steps.map((step, index) => (
               <DialogDescription className="w-11/12" key={index}>
-                <strong className="text-lg text-slate-500">{`Step ${
+                <strong className="text-base md:text-lg text-slate-500 ">{`Step ${
                   index + 1
                 }: ${step.title}`}</strong>
-                <ul>
+                <ul className="list-disc pl-6 space-y-1 mt-1">
                   {step.content.map((item, i) => (
-                    <li key={i}>{item}</li>
+                    <li key={i} className="text-sm md:text-base">
+                      {item}
+                    </li>
                   ))}
                 </ul>
               </DialogDescription>
             ))}
 
-            <DialogDescription className="w-11/12">
-              <strong className="text-slate-500">Congratulations!</strong>{" "}
-              You&apos;ve successfully completed the trade import process using
-              our conversion tool. This efficient utility ensures a hassle-free
+            <DialogDescription className=" w-11/12">
+              <strong className="text-slate-500">Congratulations!</strong> You
+              have successfully completed the trade import process using our
+              conversion tool. This efficient utility ensures a hassle-free
               experience, allowing you to focus on your trading strategies
               without the burden of manual data entry. If you encounter any
               issues or have questions, refer to the tool&apos;s documentation
