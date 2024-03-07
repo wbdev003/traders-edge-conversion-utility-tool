8df236c558c2d41a4c12a1526d3bb65105088718
Anthony Chablov <achablov@myseneca.ca>
2024-03-06 21:29:15 -0500

    style: change logo to investor edge

--- src/components/Common/Nav/Nav.tsx
+++ src/components/Common/Nav/Nav.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Container from "../Utils/Container/Container";
 import Image from "next/image";
-import logo from "./../../../../public/images/traders-edge-logo.png";
+import logo from "../../../../public/images/investor-base-logo.jpg";
 
 const Nav = () => {
   return (
--- src/components/Main/MainLayout.tsx
+++ src/components/Main/MainLayout.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Nav from "../Common/Nav/Nav";
 import Container from "../Common/Utils/Container/Container";
-import SubHeader from "../Common/SubHeader/SubHeader";
 import ProgressBarLayout from "../Common/ProgressBar/ProgressBarLayout";
-import Divider from "../Common/Divider/Divider";
 import MultiStepFormLayout from "../MultiStepForm/MultiStepFormLayout";
 import InstructionModal from "../Common/Modal/InstructionModal";
 
